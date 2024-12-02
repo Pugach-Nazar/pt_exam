5099109146273261cbc44fdf63063cf07821799a
Nazar Pugach <qwertyasd0933@gmail.com>
2024-12-02 16:07:24 +0200

    done

--- exam.py
+++ exam.py
@@ -8,5 +8,23 @@ def ar_prog_sum(a, b, n):
         res += a + i * b
     return res
 
-print(ar_prog_sum(2, 3, 4))
+
+def main():
+    a = input("Введіть n щоб вийти, будь яку клафішу щоб продовжити: ")
+    while(a != "n"):
+        try:
+            a = float(input("Введіть перший елемент прогресії: "))
+            b = float(input("Введіть різницю прогресії: "))
+            n = int(input("Введіть кількість членів прогресії: "))
+        
+            result = ar_prog_sum(a, b, n)
+            print(f"Сума перших {n} членів прогресії: {result}")
+        except ValueError as e:
+            print(f"Помилка: {e}")
+        
+        a = input("Введіть n щоб вийти, будь яку клафішу щоб продовжити: ")
+
+
+if __name__ == "__main__":
+    main()
 
